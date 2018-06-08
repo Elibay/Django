6f1990bc4329312281f6db46fbfbbd028e9a00e8
Elibay <elibay98@gmail.com>
2018-06-08 16:05:58 +0600

    something

--- firstProject/blog/views.py
+++ firstProject/blog/views.py
@@ -1,10 +1,49 @@
 from django.utils import timezone
 from django.shortcuts import render
 from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForm
 # Create your views here.
 
-
+def change_main_news():
+    for i in Post.objects.all():
+        i.main = False
+        i.save()
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(main = True)
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            if post.main == True:
+                change_main_news()
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            if post.main == True:
+                change_main_news()
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
