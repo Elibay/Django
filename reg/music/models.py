452f221ecaa3186b61acdcdbe6169d68ae4e800d
Elibay <elibay98@gmail.com>
2018-06-14 15:40:27 +0600

    jai-mai

--- reg/music/models.py
+++ reg/music/models.py
@@ -8,6 +8,8 @@ class Album(models.Model):
     album_title = models.CharField(max_length=250)
     genre = models.CharField(max_length=50)
     album_logo = models.CharField(max_length=500)
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
 
 
 class Song(models.Model):
