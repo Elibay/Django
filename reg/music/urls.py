c462fcd7e5b56f95619965418733cf29ec5027fe
Elibay <elibay98@gmail.com>
2018-06-26 10:10:42 +0600

    registration complete

--- reg/music/urls.py
+++ reg/music/urls.py
@@ -6,7 +6,10 @@ app_namespace = 'music'
 urlpatterns = [
 
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^registration/$', views.UserFormView.as_view(), name='registration'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+    url(r'album/(?P<pk>[0-9]+)/edit/$', views.AlbumEdit.as_view(), name='album-edit'),
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 ]
